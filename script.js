7727890fdbd39b0af0ae254534aef249dd58b150
Samelafarias <124313487+Samelafarias@users.noreply.github.com>
2024-04-30 08:46:52 -0300

    Atualização do HTML para receber o código Jscript , e correção nos comentários e iníco do Jscr

---
+++ script.js
@@ -0,0 +1,6 @@
+/*Java script do front-end*/
+
+//Interatividade da página de login
+//Interatividade da página principal
+//Interatividade da página de chaves
+//Interatividade da página de registros
